e3707f1258539434fa95da5c76a4e3163d9f6d5f
tuqiang01 <tuqiang01@baidu.com>
2017-11-07 10:19:43 +0800

    删除一些可能会引起安全问题的代码

--- traAccManage/config.js
+++
@@ -1,9 +0,0 @@
-const guangfa = require("./config/guangfa")
-const zhongrong = require("./config/zhongrong")
-
-module.exports = (store) => {
-    return {
-        "guangfa": guangfa(store),
-        "zhongrong":zhongrong(store)
-    };
-};
